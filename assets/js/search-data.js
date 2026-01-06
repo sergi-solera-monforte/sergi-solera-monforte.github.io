2c3cdf7ec8f56e7083d128de901814afdbf3e1c9
sergi-solera-monforte <sergi.solera.monforte@gmail.com>
2026-01-06 20:50:57 +0000

    Deploying to gh-pages from @ sergi-solera-monforte/sergi-solera-monforte.github.io@c05464e8be3397cf8f9cf7a7f4ece41e90bc0b58 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";

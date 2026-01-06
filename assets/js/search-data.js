2d60bc0a59d698659a3afb1f6455247bda13ecfb
sergi-solera-monforte <sergi.solera.monforte@gmail.com>
2026-01-06 19:57:24 +0000

    Deploying to gh-pages from @ sergi-solera-monforte/sergi-solera-monforte.github.io@af5d279d389073d78faaab23ae0686afdf86fd86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Academic teaching and laboratory instruction.",
